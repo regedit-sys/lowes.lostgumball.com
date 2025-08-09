7738db8940b923cdb4774d71dd4a60af420ffc68
Lucas <thebakokidgames@gmail.com>
2025-08-09 19:49:55 +0000

    im finna crash out

--- scripts/generate-video-index.js
+++ scripts/generate-video-index.js
@@ -5,7 +5,7 @@ const dir = join(process.cwd(), 'assets');
 const files = readdirSync(dir).filter(f => f.endsWith('.html'));
 
 const listItems = files.map(f =>
-  `<li><a href="/assets/${f}">${f}</a></li>`
+  `<li><a href="/pages/videos/uploads/${f}">${f}</a></li>`
 ).join('\n');
 
 const html = `
@@ -15,4 +15,4 @@ ${listItems}
 `;
 
 writeFileSync(join(process.cwd(), 'pages/videos/video-index.html'), html);
-console.log('Video index generated!');
+console.log('TV index generated!');
