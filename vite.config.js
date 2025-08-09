d573233719a2b8595ae5f7c44c61640f48378f3e
Lucas <thebakokidgames@gmail.com>
2025-08-09 19:28:06 +0000

    fixed build process i hope

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+    build: {
+        rollupOptions: {
+            input: {
+                main: 'index.html',
+                tv: 'pages/videos/tv-index.html',
+                video: 'pages/videos/video-index.html',
+        },
+    },
+  },
   base: '/', // for GitHub Pages
 })
