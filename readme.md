809ffaf49eaeab3b5a181e462eb27ae055968748
Lucas <thebakokidgames@gmail.com>
2025-08-09 19:58:18 +0000

    ughghhh workaround in readme

--- readme.md
+++ readme.md
@@ -3,3 +3,7 @@
 HELLo welcome to my repo for website
 
 You must access it through http://lowes.qzz.io/. Don't ask why, it only works on that.
+
+## how to access the content library
+
+<p>ts is broken, here's how to use it correctly:<br/>go to Actions on the repo, then click on the newest action that's been run. go to the deploy job, then "List dist contents." copy the file name (for example, akuma-Bm688GeS.html), and use it in this format: http://lowes.qzz.io/assets/[the filename you found]<br/>there, that should be a workaround until my dumbass finds a fix</p>
--- scripts/generate-video-index.js
+++ scripts/generate-video-index.js
@@ -1,7 +1,7 @@
 import { readdirSync, writeFileSync } from 'fs';
 import { join } from 'path';
 
-const dir = join(process.cwd(), 'assets');
+const dir = join(process.cwd(), 'pages/videos/uploads');
 const files = readdirSync(dir).filter(f => f.endsWith('.html'));
 
 const listItems = files.map(f =>
@@ -11,8 +11,9 @@ const listItems = files.map(f =>
 const html = `
 <ul>
 ${listItems}
+<p>ts is broken, here's how to use it correctly:<br/>go to Actions on the repo, then click on the newest action that's been run. go to the deploy job, then "List dist contents." copy the file name (for example, akuma-Bm688GeS.html), and use it in this format: http://lowes.qzz.io/assets/[the filename you found]<br/>there, that should be a workaround until my dumbass finds a fix</p>
 </ul>
 `;
 
-writeFileSync(join(process.cwd(), 'pages/videos/video-index.html'), html);
+writeFileSync(join(process.cwd(), 'pages/videos/tv-index.html'), html);
 console.log('TV index generated!');
